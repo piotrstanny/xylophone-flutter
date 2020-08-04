21fdf5e7370f8aa4d5f85453618408a89cb936fa
Piotr Stanny <piotr.stanny@gmail.com>
2020-08-04 16:35:33 +0100

    Create seven buttons for each sound as a row

--- lib/main.dart
+++ lib/main.dart
@@ -4,34 +4,92 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int sound) {
+    final player = AudioCache();
+    player.play('note$sound.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              textDirection: TextDirection.rtl,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                RaisedButton(
-                  color: Colors.red,
-                  padding:
-                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note1.wav');
-                  },
-                  child: Text('1'),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.red,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
+                    onPressed: () {
+                      playSound(1);
+                    },
+                  ),
                 ),
-                RaisedButton(
-                  color: Colors.orange,
-                  padding:
-                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
-                  onPressed: () {
-                    final player = AudioCache();
-                    player.play('note2.wav');
-                  },
-                  child: Text('2'),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.orange,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
+                    onPressed: () {
+                      playSound(2);
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.yellow,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
+                    onPressed: () {
+                      playSound(3);
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.lightGreen,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
+                    onPressed: () {
+                      playSound(4);
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.teal,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
+                    onPressed: () {
+                      playSound(5);
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
+                    onPressed: () {
+                      playSound(6);
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    color: Colors.deepPurpleAccent,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 90.0),
+                    onPressed: () {
+                      playSound(7);
+                    },
+                  ),
                 ),
               ],
             ),
