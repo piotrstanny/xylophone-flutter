fae3cf9fc8cb4be28bab5624d8909e10826191cf
Piotr Stanny <piotr.stanny@gmail.com>
2020-08-04 14:34:19 +0100

    Play random sound on button pressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'dart:math';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +11,16 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text('nothing yet'),
+            child: RaisedButton(
+              color: Colors.amberAccent,
+              padding: EdgeInsets.all(50.0),
+              onPressed: () {
+                final player = AudioCache();
+                int sound = Random().nextInt(7) + 1;
+                player.play('note$sound.wav');
+              },
+              child: Text('Play Sound'),
+            ),
           ),
         ),
       ),
